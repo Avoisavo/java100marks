2a447fb5d1e11bd6b2969ddf00f83c548029c39f
Tempurahere <jasonpawyl@gmail.com>
2024-05-10 17:08:36 +0800

    add on report and transaction(will be deleted)

---
+++ src/redeemitem/Report.java
@@ -0,0 +1,176 @@
+package redeemitem;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jason Paw
+ */
+// Abstract class for Report
+public abstract class Report {
+    protected String loyaltyStatus;
+    protected int totalPointsEarned;
+    protected int totalPointsRedeemed;
+    protected int totalProductRedeemed;
+    protected Map<Integer, Integer> yearlyRedeemedAmounts;
+    protected Map<Integer, Integer> pointsEarnedData;
+    
+    // Constructor
+    public Report(String loyaltyStatus, int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        this.loyaltyStatus = loyaltyStatus;
+        this.totalPointsEarned = totalPointsEarned;
+        this.totalPointsRedeemed = totalPointsRedeemed;
+        this.totalProductRedeemed = totalProductRedeemed;
+        this.yearlyRedeemedAmounts = new HashMap<>();
+    }
+    
+
+    // Abstract method to generate report
+    public abstract void generateReport();
+
+    void addYearlyRedeemedAmount(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    void addPointsEarnedData(int i, int i0) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+}
+
+// Report for Gold Status
+ class GoldStatusReport extends Report {
+    // Constructor
+    public GoldStatusReport( int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        super("Gold", totalPointsEarned, totalPointsRedeemed,totalProductRedeemed);
+    }
+    
+    // Override generateReport method
+    @Override
+    public void generateReport() {
+        System.out.println("\n---------------------------------------------------------------------------");
+        System.out.println("|                 Overall Gold Customer Yearly Report                      |");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("|Year\t|Total Points Earned\t|Total Points Redeemed\t|Total Product Redeemed");
+        System.out.println("---------------------------------------------------------------------------");
+        // Simulated data for each year
+        for (int year = 2020; year <= 2021; year++) {
+            System.out.println("|"+year + "\t|$" + totalPointsEarned+"\t\t\t|$"+totalPointsRedeemed+"\t\t\t|"+totalProductRedeemed+"\t\t\t|");
+        }
+        System.out.println("---------------------------------------------------------------------------");
+    }
+}
+
+// Report for Silver Status
+ class SilverStatusReport extends Report {
+    
+    // Constructor
+    public SilverStatusReport( int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        super("Silver", totalPointsEarned, totalPointsRedeemed,totalProductRedeemed);
+    }
+
+    // Override generateReport method
+    @Override
+    public void generateReport() {
+        System.out.println("\n---------------------------------------------------------------------------");
+        System.out.println("|                  Overall Silver Customer Yearly Report                   |");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("|Year\t|Total Amount Earned\t|Total Amount Redeemed\t|Total Product Redeemed");
+        System.out.println("---------------------------------------------------------------------------");
+        // Simulated data for each year
+        for (int year = 2020; year <= 2021; year++) {
+            System.out.println("|"+year + "\t|$" + totalPointsEarned+"\t\t\t|$"+totalPointsRedeemed+"\t\t\t|"+totalProductRedeemed+"\t\t\t|");
+        }
+        System.out.println("---------------------------------------------------------------------------");
+    }
+}
+
+// Report for Member Status
+ class MemberStatusReport extends Report {
+    // Constructor
+    public MemberStatusReport (int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        super("Member", totalPointsEarned, totalPointsRedeemed,totalProductRedeemed);
+    }
+
+    // Override generateReport method
+    @Override
+    public void generateReport() {
+        System.out.println("\n---------------------------------------------------------------------------");
+        System.out.println("|                   Overall Member Customer Yearly Report                   |");
+        System.out.println("---------------------------------------------------------------------------");
+        System.out.println("|Year\t|Total Amount Earned\t|Total Amount Redeemed\t|Total Product Redeemed");
+        System.out.println("---------------------------------------------------------------------------");
+        // Simulated data for each year
+        for (int year = 2020; year <= 2021; year++) {
+            System.out.println("|"+year + "\t|$" + totalPointsEarned+"\t\t\t|$"+totalPointsRedeemed+"\t\t\t|"+totalProductRedeemed+"\t\t\t|");
+        }
+        System.out.println("---------------------------------------------------------------------------");
+    }
+}
+
+// Yearly Redemption Trends Report
+ class RedemptionTrendsReport extends Report {
+    
+    // Constructor
+    public RedemptionTrendsReport( int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        super("", totalPointsEarned, totalPointsRedeemed,totalProductRedeemed);
+    }
+    @Override
+    public void addYearlyRedeemedAmount(int year, int redeemedAmount) {
+        yearlyRedeemedAmounts.put(year, redeemedAmount);
+    }
+    // Override generateReport method
+    @Override
+    public void generateReport() {
+        System.out.println("\n------------------------------");
+        System.out.println("|Points Redemption Trends Report|");
+        System.out.println("------------------------------");
+        System.out.println("|Year\t|Redeemed Amount\t|");
+        System.out.println("------------------------------");
+        for (Map.Entry<Integer, Integer> entry : yearlyRedeemedAmounts.entrySet()) {
+            int year = entry.getKey();
+            int redeemedAmount = entry.getValue();
+            System.out.println("|" + year + "\t|$" + redeemedAmount + "\t\t\t|");
+        }
+        System.out.println("------------------------------");
+    }
+}
+
+// Yearly Points Earned Trends Report
+ class PointsEarnedTrendsReport extends Report {
+    // Constructor
+    public PointsEarnedTrendsReport(int totalPointsEarned, int totalPointsRedeemed,int totalProductRedeemed) {
+        super("",totalPointsEarned, totalPointsRedeemed,totalProductRedeemed);
+        this.pointsEarnedData = new HashMap<>();
+    }
+
+    // Method to add points earned data for a specific year
+
+    public void addPointsEarnedData(int year, int pointsEarned) {
+        pointsEarnedData.put(year, pointsEarned);
+    }
+
+    // Override generateReport method
+    @Override
+    public void generateReport() {
+      
+        System.out.println("\n----------------------------");
+        System.out.println("|Points Earned Trends Report|");
+        System.out.println("----------------------------");
+        System.out.println("|Year\t|Points Earned\t|");
+        System.out.println("----------------------------");
+        // Display points earned data for each year
+        for (Map.Entry<Integer, Integer> entry : pointsEarnedData.entrySet()) {
+            System.out.println("|" + entry.getKey() + "\t|" + entry.getValue() + "\t\t|");
+        }
+        
+        System.out.println("------------------------------");
+    }
+}
---
+++ src/redeemitem/Transaction.java
@@ -0,0 +1,75 @@
+package redeemitem;
+
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jason Paw
+ */
+/*
+// Transaction class
+public class Transaction {
+    private static int transactionIdCounter = 1; // Counter for auto-generated transaction ID
+    private String transactionId;
+    private String customerName;
+    private int points;
+    private Date transactionDate ;
+    private String transactionType;
+
+    // Constructor
+    public Transaction(String customerName, int points, String transactionType) {
+        this.transactionId = generateTransactionId(); // Auto-generated transaction ID
+        this.customerName = customerName;
+        this.points = points;
+        this.transactionDate = new Date();  // Current date
+        this.transactionType = transactionType;
+    }
+    
+    private String generateTransactionId() {
+        String id = String.format("%04d", transactionIdCounter);
+        transactionIdCounter++;
+        return id;
+    }
+    
+    // Getters
+    public String getTransactionId() {
+        return transactionId;
+    }
+    public String getCustomerName() {
+        return customerName;
+    }
+    public int getPoints() {
+        return points;
+    }
+    public Date getTransactionDate() {
+        return transactionDate;
+    }
+    public String getTransactionType() {
+        return transactionType;
+    }
+    
+    // Method to display transactions in table format
+    public static void displayTransactions(List<Transaction> transactions) {
+        System.out.println("-------------------------------------------------------------------");
+        System.out.println("|                        TRANSACTION DETAILS                      |");
+        System.out.println("------------------------------------------------------------------");
+        System.out.println("| Transaction ID | Customer Name | Points |  Date     | Type      |");
+        System.out.println("-------------------------------------------------------------------");
+        for (Transaction transaction : transactions) {
+            System.out.printf("| %-15s| %-14s| %-7d| %-8tF | %-10s|\n",
+                    transaction.getTransactionId(), transaction.getCustomerName(),
+                    transaction.getPoints(), transaction.getTransactionDate(),
+                    transaction.getTransactionType());
+        }
+        System.out.println("-------------------------------------------------------------------\n");
+    }
+}*/
